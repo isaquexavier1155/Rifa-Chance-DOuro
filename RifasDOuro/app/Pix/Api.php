2c5fbefca09dd7a2c3d00767e3e8fbfab86c54c4
isaquexavier1155 <isaque.ixs@gmail.com>
2024-01-15 15:31:39 -0300

    Ajustes finais em produção

--- RifasDOuro/app/Pix/Api.php
+++ RifasDOuro/app/Pix/Api.php
@@ -113,7 +113,8 @@ class Api
       CURLOPT_SSLCERTPASSWD  => '',
       // CURLOPT_CAINFO => 'C:\Users\isaqu\OneDrive\Documentos\Clone Git Hub Projeto Integração Api PIX com Atualização de Status\wdev-qrcode-pix-php\files\certificates\cacert.pem',
       //CURLOPT_CAINFO => 'storage/files/certificates/cacert.pem',
-      CURLOPT_CAINFO => base_path('public_html/Rifa-Chance-DOuro/RifasDOuro/storage/files/certificates/cacert.pem'),
+      //CURLOPT_CAINFO => base_path('public_html/Rifa-Chance-DOuro/RifasDOuro/storage/files/certificates/cacert.pem'),
+      CURLOPT_CAINFO => '/home1/rifad375/public_html/Rifa-Chance-DOuro/RifasDOuro/storage/files/certificates/cacert.pem',
       CURLOPT_SSL_VERIFYPEER => true,
       CURLOPT_HTTPHEADER     => $headers
     ]);
@@ -191,7 +192,8 @@ if ($responseArray === null && json_last_error() !== JSON_ERROR_NONE) {
           CURLOPT_SSLCERTPASSWD  => '',
           // CURLOPT_CAINFO => 'C:\Users\isaqu\OneDrive\Documentos\Sistema de Rifas- em Andamento - Junção de Projeto Eventos com Integração PIX PHP\RifasDOuro\storage\files\certificates\cacert.pem',
           //CURLOPT_CAINFO => 'storage/files/certificates/cacert.pem',
-          CURLOPT_CAINFO => base_path('public_html/Rifa-Chance-DOuro/RifasDOuro/storage/files/certificates/cacert.pem'),
+          //CURLOPT_CAINFO => base_path('public_html/Rifa-Chance-DOuro/RifasDOuro/storage/files/certificates/cacert.pem'),
+          CURLOPT_CAINFO => '/home1/rifad375/public_html/Rifa-Chance-DOuro/RifasDOuro/storage/files/certificates/cacert.pem',
           CURLOPT_SSL_VERIFYPEER => true,
           CURLOPT_HTTPHEADER     => $headers
       ]);
